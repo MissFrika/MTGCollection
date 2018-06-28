56497c01ada7d734d888262faf82a31d02bae190
Frida <FS_Earthbound@hotmail.com>
2018-06-28 15:37:42 +0200

    New project

---
+++ MTGCollection/MTGCollection/MTGCollection/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MTGCollection.Models;
+
+namespace MTGCollection.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
