7561f46f07d7393700a534c902baf43be8c712d5
Frida <FS_Earthbound@hotmail.com>
2018-07-01 13:23:40 +0200

    CardViewModel

---
+++ MTGCollection/MTGCollection/MTGCollection/ViewModels/CardDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MTGCollection.Models;
+
+namespace MTGCollection.ViewModels
+{
+    class CardDetailViewModel : BaseViewModel
+    {
+    public Card Card { get; set; }
+
+        public CardDetailViewModel(Card card = null)
+        {
+            Title = card?.CardName;
+            Card = card;
+        }
+    }
+}
